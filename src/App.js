ea151b0399d7be681a362b75f814a9e35bd5220c
Darren-ODonnell <Dar@ren01>
2023-03-10 20:55:16 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import TeamBuilder from "./archive/TeamBuilder";
 import PlayerList from "./archive/Draggable";
 import Droppable from "./archive/Droppable";
 import Example from "./dragndrop/DragnDropExample";
-
+const names = [
+    "John Smith",
+    "Emma Johnson",
+    "Michael Davis",
+    "Sarah Williams",
+    "William Brown",
+    "Olivia Wilson",
+    "James Garcia",
+    "Ava Rodriguez",
+    "David Martinez",
+    "Sophia Anderson",
+    "Joseph Taylor",
+    "Emily Jackson",
+    "Christopher White",
+    "Isabella Harris",
+    "Matthew Martin",
+];
 
 function App() {
   return (
