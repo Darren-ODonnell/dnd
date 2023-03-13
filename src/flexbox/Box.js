173583c6af62a88cffc18ab2db559d4579d3d75c
Darren Brendan O'Donnell <61713213+Darren-ODonnell@users.noreply.github.com>
2023-03-13 16:17:24 +0000

    Teamsheet updated

--- src/flexbox/Box.js
+++ src/flexbox/Box.js
@@ -19,14 +19,15 @@ const BoxWrapper = styled.div`
   opacity: ${(props) => (props.isDragging ? 0.5 : 1)};
 `;
 
-const Box = ({ width, height, x, y, text, onDrop }) => {
+const Box = ({ width, height, x, y, id, name, onDrop }) => {
     const ref = useRef(null);
 
     const [{ isDragging }, drag] = useDrag(() => ({
         type: "ITEM",
-        item: { text },
+        item: { name },
         collect: (monitor) => ({
             isDragging: monitor.isDragging(),
+            // canDrop: monitor.canDrop(),
         }),
     }));
 
@@ -36,9 +37,12 @@ const Box = ({ width, height, x, y, text, onDrop }) => {
             const delta = monitor.getDifferenceFromInitialOffset();
             const left = Math.round(x + delta.x);
             const top = Math.round(y + delta.y);
-            const newBox = { left, top, text };
+            const newBox = { left, top, id };
             // Call the onDrop function with the newBox object
-            onDrop(newBox);
+
+            console.log("player id: "+id+ " Player name: "+name)
+
+            onDrop(newBox, id);
         },
         collect: (monitor) => ({
             isOver: monitor.isOver(),
@@ -74,91 +78,10 @@ const Box = ({ width, height, x, y, text, onDrop }) => {
             isDragging={isDragging}
             isOver={isOver}
         >
-            {text}
+            {name}
         </BoxWrapper>
     );
 };
 
 export default Box;
-//
-//
-// const BoxWrapper = styled.div`
-//   position: absolute;
-//   width: ${(props) => props.width}px;
-//   height: ${(props) => props.height}px;
-//   top: ${(props) => props.y}px;
-//   left: ${(props) => props.x}px;
-//   background-color: lightblue;
-//   border-radius: 10px;
-//   border: 5px solid lightblue;
-//   display: flex;
-//   font-size: 20px;
-//   cursor: move;
-//   draggable: true;
-//   background-color: ${(props) => (props.isOver ? "lightblue" : "lightgray")};
-//   opacity: ${(props) => (props.isDragging ? 0.5 : 1)};
-// `;
-//
-// const Box = ({ width, height, x, y, text, onDrop }) => {
-//     const ref = useRef(null);
-//
-//     const [{ isDragging }, drag] = useDrag(() => ({
-//         type: "ITEM",
-//         item: { text },
-//         collect: (monitor) => ({
-//             isDragging: monitor.isDragging(),
-//         }),
-//     }));
-//
-//     const [{ canDrop, isOver }, drop] = useDrop(() => ({
-//         accept: "ITEM",
-//         drop: (item, monitor) => {
-//             const delta = monitor.getDifferenceFromInitialOffset();
-//             const left = Math.round(x + delta.x);
-//             const top = Math.round(y + delta.y);
-//             const newBox = { left, top, text };
-//             // Pass the newBox object to the BoxContainer component
-//             return newBox;
-//         },
-//         collect: (monitor) => ({
-//             isOver: monitor.isOver(),
-//             canDrop: monitor.canDrop(),
-//         }),
-//     }));
-//
-//     useEffect(() => {
-//         const node = ref.current;
-//         drag(drop(node));
-//     }, [drag, drop]);
-//
-//     const styles = {
-//         position: "absolute",
-//         width: `${width}px`,
-//         height: `${height}px`,
-//         top: `${y}px`,
-//         left: `${x}px`,
-//         backgroundColor: "lightgray",
-//         borderRadius: "10px",
-//         border: "5px solid lightblue",
-//         display: "flex",
-//         fontSize: "24px",
-//         cursor: "move",
-//         opacity: isDragging ? 0.5 : 1,
-//     };
-//
-//     return (
-//         <BoxWrapper
-//             ref={ref}
-//             style={styles}
-//             isDragging={isDragging}
-//             isOver={isOver}
-//         >
-//             {text}
-//         </BoxWrapper>
-//         // <div ref={ref} style={styles}>
-//         //     {text}
-//         // </div>
-//     );
-// };
-//
-// export default Box;
+
--- src/flexbox/FlexBox2.js
+++ src/flexbox/FlexBox2.js
@@ -9,22 +9,28 @@ import './flexbox2.css'
 import grassTexture from '../media/Background-Grass.png';
 import BoxContainer from "./BoxContainer";
 
-const players = [
-    { id: 1, name: "John Smith", position: "Forward" },
-    { id: 2, name: "Mary Johnson", position: "Midfielder" },
-    { id: 3, name: "James Williams", position: "Defender" },
-    { id: 4, name: "Elizabeth Brown", position: "Goalkeeper" },
-    { id: 5, name: "David Garcia", position: "Forward" },
-    { id: 6, name: "Sarah Lee", position: "Midfielder" },
-    { id: 7, name: "Michael Martin", position: "Defender" },
-    { id: 8, name: "Emily Davis", position: "Goalkeeper" },
-    { id: 9, name: "Christopher Rodriguez", position: "Forward" },
-    { id: 10, name: "Ashley Martinez", position: "Midfielder" },
-    { id: 11, name: "Matthew Hernandez", position: "Defender" },
-    { id: 12, name: "Samantha Lopez", position: "Goalkeeper" },
-    { id: 13, name: "Joshua Gonzalez", position: "Forward" },
-    { id: 14, name: "Amanda Taylor", position: "Midfielder" },
-    { id: 15, name: "Kevin Phillips", position: "Defender" },
+const panelList = [
+    { id: 1 , name: "John Smith"           , position: "Forward" },
+    { id: 2 , name: "Mary Johnson"         , position: "Midfielder" },
+    { id: 3 , name: "James Williams"       , position: "Defender" },
+    { id: 4 , name: "Elizabeth Brown"      , position: "Goalkeeper" },
+    { id: 5 , name: "David Garcia"         , position: "Forward" },
+    { id: 6 , name: "Sarah Lee"            , position: "Midfielder" },
+    { id: 7 , name: "Michael Martin"       , position: "Defender" },
+    { id: 8 , name: "Emily Davis"          , position: "Goalkeeper" },
+    { id: 9 , name: "Christopher Rodriguez", position: "Forward" },
+    { id: 10, name: "Ashley Martinez"      , position: "Midfielder" },
+    { id: 11, name: "Matthew Hernandez"    , position: "Defender" },
+    { id: 12, name: "Samantha Lopez"       , position: "Goalkeeper" },
+    { id: 13, name: "Joshua Gonzalez"      , position: "Forward" },
+    { id: 14, name: "Amanda Taylor"        , position: "Midfielder" },
+    { id: 15, name: "Kevin Phillips"       , position: "Defender" },
+    { id: 16, name: "Aubrey Martinez"      , position: "Midfielder" },
+    { id: 17, name: "Mark Hernandez"       , position: "Defender" },
+    { id: 18, name: "Sam Lopez"            , position: "Goalkeeper" },
+    { id: 19, name: "Jimmy Gonzalez"       , position: "Forward" },
+    { id: 20, name: "Joan Taylor"          , position: "Midfielder" },
+    { id: 21, name: "Paul Phillips"        , position: "Defender" },
 ];
 
 
@@ -33,18 +39,14 @@ const Layout = ({data}) => {
     const [team, setTeam] = useState([])
     const [subs, setSubs] = useState([])
     const [panel, setPanel] = useState([])
-    const [players1, setPlayers1] = useState([])
+    const [players, setPlayers] = useState([])
     const [teamsheet, setTeamsheet] = useState([])
     const [fixtureDate, setFixtureDate] = useState(new Date())
     // const [data, error, loading, axiosApi] = useAxios()
 
-
-
     const handleTeamChange = (e) => {
-
     }
     const handleSubsChange = (e) => {
-
     }
     const handlePanelChange = (e) => {
 
@@ -69,7 +71,7 @@ const Layout = ({data}) => {
     }
 
     useEffect(() => {
-        setPlayers1( getPlayers() )
+        setPlayers(getPlayers() )
         setTeamsheet( getTeamsheet() )
         setFixtureDate( getFixtureDate() )
         setTeam( getTeam(teamsheet) )
@@ -90,9 +92,9 @@ const Layout = ({data}) => {
     const margin = spacer
     let startRow = margin // creates clear space at the top of the container
 
-    const dragColWidth = width + 30 // width of the left and right columns
+    const dragColWidth = margin + width + 30 // width of the left and right columns
 
-    const threeColStart = dragColWidth + spacer
+    const threeColStart = dragColWidth + spacer + margin
     const twoColStart = dragColWidth + spacer * 3
     const keeperColStart = dragColWidth + spacer * 4
     const keeperRow = 20
@@ -149,23 +151,31 @@ const Layout = ({data}) => {
         console.log("FlexBox : Cancel Close Clicked")
     }
 
-    const handleDrop = (data) => {
+    const handleDrop = (data, source) => {
         // Handle the data that was dropped
-        console.log(data);
+        // determine what dropped the object
+        // remove the name from that object
+        // is there an object already there in box object
+        // remove object from box object
+        // add new object to box
+        // add removed object from Panel / Subs
+        // add removed object from box to Panel
+
+        console.log("Data: "+data+" Source: "+source);
     }
 
     return (
         <>
             <Container>
                 {/*<SingleBox  colStart = {panelStartCol}  rowStart = {panelStartRow}                   height   = {height * cells-7 } width = {dragColWidth} posn = {"Panel"} gap={spacer}/>*/}
-                <BoxContainer maxComponents={80} canDrag={false} players={players} x={0} y={0} onDrop={handleDrop}/>
+                <BoxContainer maxComponents={80} canDrag={false} players={panelList} x={0} y={0} onDrop={handleDrop}/>
 
-                <RowKeeper  colStart = {margin + dragColWidth + spacer * 6} rowStart = {startRow}                        height   = {height} width = {width} posn = {1} gap={spacer}/>
-                <RowOfThree colStart = {margin + threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {2} gap={spacer} onDrop={handleDrop}/>
-                <RowOfThree colStart = {margin + threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {5} gap={spacer} onDrop={handleDrop}/>
-                <RowOfTwo   colStart = {margin + twoColStart}    rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {8} gap={spacer} onDrop={handleDrop}/>
-                <RowOfThree colStart = {margin + threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {10} gap={spacer} onDrop={handleDrop}/>
-                <RowOfThree colStart = {margin + threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {13} gap={spacer} onDrop={handleDrop}/>
+                <RowKeeper  colStart = {dragColWidth + spacer * 6} rowStart = {startRow}                        height   = {height} width = {width} posn = {1} gap={spacer}/>
+                <RowOfThree colStart = {threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {2} gap={spacer} onDrop={handleDrop}/>
+                <RowOfThree colStart = {threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {5} gap={spacer} onDrop={handleDrop}/>
+                <RowOfTwo   colStart = {twoColStart}    rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {8} gap={spacer} onDrop={handleDrop}/>
+                <RowOfThree colStart = {threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {10} gap={spacer} onDrop={handleDrop}/>
+                <RowOfThree colStart = {threeColStart}  rowStart = {startRow  += rowSpace + height}  height   = {height} width = {width} posn = {13} gap={spacer} onDrop={handleDrop}/>
                 {/*<BoxContainer maxComponents={80} canDrag={false} players={players} x={8000} y={0}/>*/}
                 <SingleBox  colStart = {margin + subsStartCol - 40}   rowStart = {subsStartRow}                    height   = {colHeight -7 } width = {dragColWidth} posn = {"Subs"} gap={spacer} onDrop={handleDrop}/>
 
