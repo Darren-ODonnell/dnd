84dedc8a3e0c1fe7a477df2dbb866c55fcc32c4c
Darren-ODonnell <Dar@ren01>
2023-03-21 23:14:22 +0000

    initial

--- src/SCRATCH.js
+++ src/SCRATCH.js
@@ -1,59 +1,11 @@
-Ok I have reduced the Teanmsheet Container code
-My problem is the same - When I update the screen the data that the Box gets does not change
+Tidyup extra variables passed to/from Box and TeamsheetContainer
 
+Panel
+GW - HK
+GW - IC - Crash
 
+Subs
+CL - ST
+CL - EH Charlie doubled on 2nd / 3rd EH at top CL gone.
 
-
-const TeamsheetContainer = ({panel,team, subs, onDrop}) => {
-    const handleDrop = (box, containerId) => {
-        // handle the drop event
-        console.log("Box dropped into container", containerId);
-        onDrop()
-    };
-    // const [source, setSource] = useState(panel);
-
-    // useEffect(() => {
-    //     setSource(panel);
-    // }, [panel]);
-
-    console.log("Panel: TeamsheetContainer() - " + panel.map(m => {return m.name + " - "}))
-
-    return (
-        <Container className="teamsheet-container container mx-auto" style={{height:'800px'}}>
-            <PanelContainer  panel  = {panel} onDrop = {onDrop}/>
-            {/*<TeamContainer   team   = {team}  onDrop = {onDrop}/>*/}
-            {/*<SubsContainer   subs   = {subs}  onDrop = {onDrop}/>*/}
-            {/*<ActionContainer                  />*/}
-        </Container>
-    )
-}
-export default TeamsheetContainer;
-
-const PanelContainer = ({ panel, onDrop }) => {
-    let nextRow = 0
-    console.log("Panel: inside  PanelContainer() - " + panel.map(m => {return m.name + " - "}))
-    return (
-
-        <Container className="panel-container" >
-            { panel.map(( member ) => {
-                const top = nextRow;
-                nextRow += 60; // Increment nextRow by 50 for the next iteration
-                console.log("Panel: inside  PanelContainer() - " + panel.map(m => {return m.name + " - "}))
-                return (
-                    <Box
-                        key={member.id}
-                        id={member.id}
-                        player={member}
-                        width={150}
-                        height={50}
-                        x={0}
-                        y={top}
-                        onDrop={onDrop}
-                        style={{marginLeft: "5px"}}
-                        source={panel}
-                    />
-                );
-            })}
-        </Container>
-    );
-};
+Add functionality to Cancel and Save Buttons
